fa109dbcd5abeb5f2a52e796141ebb1ec6c97110
Bystroushaak <bystrousak@kitakitsune.org>
2015-11-24 04:49:53 +0100

    Switched to complete randomly generated ZEO environment.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+0.2.1
+-----
+    - Switched to complete randomly generated ZEO environment (random name in /tmp, and random ports for ZEO server process).
+
 0.2.0
 -----
     - Bugfix of path parsing.
--- src/zeo_connector_defaults/environment_generator.py
+++ src/zeo_connector_defaults/environment_generator.py
@@ -5,6 +5,7 @@
 #
 # Imports =====================================================================
 import shutil
+import random
 import os.path
 import tempfile
 import threading
@@ -13,47 +14,75 @@ from string import Template
 
 
 # Variables ===================================================================
-SERV = None
-TMP_DIR = None
+SERV = None  #: Here is stored the running process.
+TMP_PATH = None  #: Here will be stored the path to the temporary directory.
+ZEO_SERVER = "localhost"  #: Hostname for the ZEO server
+ZEO_PORT = random.randint(20000, 65000)  #: Port for the ZEO server
 
 
 # Functions & classes =========================================================
 def data_context_name(fn):
-    return os.path.join(os.path.dirname(__file__), "test_data", fn)
+    """
+    Return the `fn` in absolute path in `template_data` directory.
+    """
+    return os.path.join(os.path.dirname(__file__), "template_data", fn)
 
 
 def data_context(fn, mode="r"):
+    """
+    Return content fo the `fn` from the `template_data` directory.
+    """
     with open(data_context_name(fn), mode) as f:
         return f.read()
 
 
 def tmp_context_name(fn=None):
+    """
+    Return the `fn` in absolute path in temporary directory.
+    """
     if not fn:
-        return TMP_DIR
+        return TMP_PATH
 
-    return os.path.join(TMP_DIR, fn)
+    return os.path.join(TMP_PATH, fn)
 
 
 def tmp_context(fn, mode="r"):
+    """
+    Return content fo the `fn` from the temporary directory.
+    """
     with open(tmp_context_name(fn), mode) as f:
         return f.read()
 
 
 # Environment generators ======================================================
 def generate_environment():
-    global TMP_DIR
-    TMP_DIR = tempfile.mkdtemp()
+    """
+    Generate the environment in ``/tmp`` and run the ZEO server process in
+    another thread.
+    """
+    global TMP_PATH
+    TMP_PATH = tempfile.mkdtemp()
 
     # write ZEO server config to  temp directory
-    zeo_conf_path = os.path.join(TMP_DIR, "zeo.conf")
+    zeo_conf_path = os.path.join(TMP_PATH, "zeo.conf")
     with open(zeo_conf_path, "w") as f:
         f.write(
-            Template(data_context("zeo.conf")).substitute(path=TMP_DIR)
+            Template(data_context("zeo.conf")).substitute(
+                path=TMP_PATH,
+                server=ZEO_SERVER,
+            )
         )
 
     # write client config to temp directory
-    client_config_path = os.path.join(TMP_DIR, "zeo_client.conf")
-    shutil.copy(data_context_name("zeo_client.conf"), client_config_path)
+    client_config_path = os.path.join(TMP_PATH, "zeo_client.conf")
+    with open(client_config_path, "w") as f:
+        f.write(
+            Template(data_context("zeo_client.conf")).substitute(
+                path=TMP_PATH,
+                server=ZEO_SERVER,
+                port=ZEO_PORT,
+            )
+        )
 
     # run the ZEO server
     def run_zeo():
@@ -66,10 +95,14 @@ def generate_environment():
 
 
 def cleanup_environment():
+    """
+    Shutdown the ZEO server process running in another thread and cleanup the
+    temporary directory.
+    """
     SERV.terminate()
-    shutil.rmtree(TMP_DIR)
-    if os.path.exists(TMP_DIR):
-        os.rmdir(TMP_DIR)
+    shutil.rmtree(TMP_PATH)
+    if os.path.exists(TMP_PATH):
+        os.rmdir(TMP_PATH)
 
-    global TMP_DIR
-    TMP_DIR = None
+    global TMP_PATH
+    TMP_PATH = None
